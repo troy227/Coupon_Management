16e0f2fbbf640d04bac89ced46053553169c1616
troy227 <tushar.roy@niyotail.com>
2021-12-14 11:45:03 +0530

    Added auth and middleware

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(Auth::user()->user_type == 'Admin')
+        {
+            return $next($request);
+        }
+        return abort(403, 'Unauthorized action.');
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/coupons', 'CouponController@index');
 Route::get('/coupons/create', 'CouponController@create');
-Route::post('/coupons/store', 'CouponController@store');
-Route::get('/coupons/{coupon}', 'CouponController@show');
-Route::post('/coupons/{coupon}/delete', 'CouponController@delete');
-Route::get('/coupons/{coupon}/edit', 'CouponController@edit');
-Route::post('/coupons/{coupon}/update', 'CouponController@update');
+
+Route::get('/home', 'HomeController@index')->name('home');
+Auth::routes();
+Route::middleware(['auth'])->group(function () {
+    Route::get('/coupons', 'CouponController@index');
+    Route::get('/coupons/{coupon}', 'CouponController@show');
+
+});
+
+Route::middleware(['auth', 'isAdmin'])->group(function () {
+
+    Route::post('/coupons/store', 'CouponController@store');
+    Route::post('/coupons/{coupon}/delete', 'CouponController@delete');
+    Route::get('/coupons/{coupon}/edit', 'CouponController@edit');
+    Route::post('/coupons/{coupon}/update', 'CouponController@update');
+});
+
+
